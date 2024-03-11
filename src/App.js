8a233b0343f94d16ebafe325f0c3769b962fcdb7
bethjm <b.jeanne.mills@gmail.com>
2024-03-11 18:28:23 -0400

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+
+import LogIn from "./components/LogIn";
+import Home from "./components/Home";
+import DealsTab from "./components/DealsTab";
+import ConnectionTab from "./components/ConnectionsTab";
+import Territory from "./components/Territory";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="App">
-      {/* put the nav bar here */}
-      <h1>App</h1>
-    </div>
+    <>
+      <Switch>
+        <Route path="/" exact component={LogIn} />
+        <Route path="/home" component={Home} />
+        <Route path="/deals" component={DealsTab} />
+        <Route path="/connections" component={ConnectionTab} />
+        <Route path="/territory" component={Territory} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    </>
   );
 }
 
