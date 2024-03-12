33ede952a6ed28cde9d17178381f7f83f80d1df9
bethjm <b.jeanne.mills@gmail.com>
2024-03-12 18:57:25 -0400

    elements in correct place for log in page

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,11 +1,41 @@
+import "./LogIn.css";
+
 import React from "react";
 
 function LogIn() {
   return (
-    <>
-      <p>hi from</p>
-      <p>login</p>
-    </>
+    <div className="LogIn">
+      <div className="LogIn_Container">
+        <div className="header">
+          <h1>HōmHub</h1>
+          <h3>Mortgage Provider's Internet Marketing Solution</h3>
+        </div>
+        <div className="login_info">
+          <div className="existing_user">
+            <h3>User Login</h3>
+
+            <p>hi</p>
+            <p>hi</p>
+            <p>hi</p>
+            <p>hi</p>
+            <p>hi</p>
+            <p>hi</p>
+          </div>
+          <div className="line">
+            <h1></h1>
+          </div>
+          <div className="create_user">
+            <h3>Create Account</h3>
+            <p>hi</p>
+            <p>hi</p>
+            <p>hi</p>
+            <p>hi</p>
+            <p>hi</p>
+            <p>hi</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
