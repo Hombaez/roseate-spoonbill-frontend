8552d3eb307c3d82a30744e82a0f13aacb0a830a
bethjm <b.jeanne.mills@gmail.com>
2024-03-12 19:54:26 -0400

    formatting for page positioning

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -2,6 +2,8 @@ import "./LogIn.css";
 
 import React from "react";
 
+import PrimaryButton from "./UI/buttons/PrimaryButton";
+
 function LogIn() {
   return (
     <div className="LogIn">
@@ -14,24 +16,57 @@ function LogIn() {
           <div className="existing_user">
             <h3>User Login</h3>
 
-            <p>hi</p>
-            <p>hi</p>
-            <p>hi</p>
-            <p>hi</p>
-            <p>hi</p>
-            <p>hi</p>
+            <form
+              action="your_server_endpoint"
+              method="POST"
+              className="existing_user"
+            >
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                className="input_styling"
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="input_styling"
+              />
+              <PrimaryButton>Log in</PrimaryButton>
+            </form>
+            <a>forgot password?</a>
           </div>
-          <div className="line">
-            <h1></h1>
-          </div>
+          <div className="line"></div>
           <div className="create_user">
-            <h3>Create Account</h3>
-            <p>hi</p>
-            <p>hi</p>
-            <p>hi</p>
-            <p>hi</p>
-            <p>hi</p>
-            <p>hi</p>
+            <h3>Create an Account</h3>
+
+            <form
+              action="your_server_endpoint"
+              method="POST"
+              className="create_user"
+            >
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                className="input_styling"
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="input_styling"
+              />
+              <input
+                type="password"
+                name="verify_password"
+                placeholder="Verify Password"
+                className="input_styling"
+              />
+              <PrimaryButton>Create User</PrimaryButton>
+            </form>
           </div>
         </div>
       </div>
