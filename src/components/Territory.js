d6b012fb0a6970cc1d4c6ec6a5461575e210e2c4
bethjm <b.jeanne.mills@gmail.com>
2024-03-13 19:16:12 -0400

    zip codes in order

--- src/components/Territory.js
+++ src/components/Territory.js
@@ -7,22 +7,31 @@ function Territory() {
     <div className="Territory">
       <div className="map">map</div>
       <div className="zip_codes">
+        {/* current work area */}
         <div className="your_zip">
-          <div className="your_zip_title">
-            <h3>Your Territory</h3>
-          </div>
-          <div className="all_zip_data">
-            <div className="zip_data">
-              <h2>zip</h2>
-              <h2>02346</h2>
-              <h2>02134</h2>
-            </div>
-            <div className="impressions">
-              <h2>impression & queue</h2>
-              <h2>17%</h2>
-              <h2>21%</h2>
-            </div>
-          </div>
+          <div className="your_zip_title">Your Territory</div>
+          <table>
+            <tr>
+              <th>Zip Code</th>
+              <th>Impressions</th>
+            </tr>
+            <tr>
+              <td>02116</td>
+              <td>17%</td>
+            </tr>
+            <tr>
+              <td>02134</td>
+              <td>34%</td>
+            </tr>
+            <tr>
+              <td>02346</td>
+              <td>12%</td>
+            </tr>
+            <tr>
+              <td>02111</td>
+              <td>3%</td>
+            </tr>
+          </table>
         </div>
         {/* current work area */}
         <div className="buy_zip">
@@ -33,27 +42,34 @@ function Territory() {
           <div className="search_bar">
             <h3>this is a search bar</h3>
           </div>
-          <div className="available_zips">
-            <div className="all_zip_data">
-              <div className="zip_data">
-                <h2>zip</h2>
-                <h2>02346</h2>
-                <h2>02134</h2>
-              </div>
-              <div className="availability_buttons">
-                <h3>availability</h3>
-                <h3>this is a button</h3>
-                <h3>this is a button</h3>
-              </div>
-              <div className="impressions">
-                <h2>impression & queue</h2>
-                <h2>17%</h2>
-                <h2>21%</h2>
-              </div>
-            </div>
-          </div>
+          <table>
+            <tr>
+              <th>Zip Code</th>
+              <th>Availability</th>
+              <th>Impressions</th>
+            </tr>
+            <tr>
+              <td>02116</td>
+              <td>Button</td>
+              <td>17%</td>
+            </tr>
+            <tr>
+              <td>02134</td>
+              <td>Button</td>
+              <td>34%</td>
+            </tr>
+            <tr>
+              <td>02346</td>
+              <td>Button</td>
+              <td>12%</td>
+            </tr>
+            <tr>
+              <td>02111</td>
+              <td>Button</td>
+              <td>3%</td>
+            </tr>
+          </table>
         </div>
-        {/* current work area */}
       </div>
     </div>
   );
