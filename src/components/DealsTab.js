c3e02f1a2992768a27da24c8d2e1cf0060f73814
bethjm <b.jeanne.mills@gmail.com>
2024-03-13 08:44:39 -0400

    basic deals and basic importnant info in place

--- src/components/DealsTab.js
+++ src/components/DealsTab.js
@@ -1,11 +1,40 @@
+import "./DealsTab.css";
+
 import React from "react";
+
+import GreenButton from "./UI/buttons/GreenButton";
 
 function DealsTab() {
   return (
-    <>
-      <p>hi from</p>
-      <p>DealsTab</p>
-    </>
+    <div className="DealsTab">
+      <div className="DealsTab_Container">
+        <div className="deals_details">
+          <div className="new_deals">
+            <h2>New Deals</h2>
+            <p>content</p>
+          </div>
+          <div className="expired_deals">
+            <h2>Expired Deals</h2>
+            <p>content</p>
+          </div>
+          <div className="closed_deals">
+            <h2>Closed Deals</h2>
+            <p>content</p>
+          </div>
+        </div>
+        <div className="important_info">
+          <h4>2 New Docs</h4>
+          <h4>5 New Messages</h4>
+          <h4>1 Upcoming Dates</h4>
+        </div>
+        {/* current work area */}
+        <div className="active_deals">
+          <h1>Active deals</h1>
+          <p>hi</p>
+        </div>
+        {/* current work area */}
+      </div>
+    </div>
   );
 }
 
