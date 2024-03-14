04a64c902969d7c09a297ed2b190e97816bf3915
bethjm <b.jeanne.mills@gmail.com>
2024-03-14 10:34:46 -0400

    grid set up

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,18 @@
+import "./Profile.css";
 import React from "react";
 
 function Profile() {
   return (
-    <>
-      <p>hi from</p>
-      <p>Profile</p>
-    </>
+    <div className="Profile">
+      {/* current working area */}
+      <div className="accountdetails">accountdetails</div>
+      {/* current working area */}
+      <div className="settings">settings</div>
+      <div className="statements">statements</div>
+      <div className="payment">payment</div>
+      <div className="support">support</div>
+      <div className="billing">billing</div>
+    </div>
   );
 }
 
