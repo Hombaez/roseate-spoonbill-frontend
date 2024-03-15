81e55216f1835e71ad9a5c3afcfe8e979bc78b2a
bethjm <b.jeanne.mills@gmail.com>
2024-03-15 17:17:04 -0400

    dealsTab

--- src/components/DealsTab.js
+++ src/components/DealsTab.js
@@ -6,6 +6,83 @@ import { useNavigate } from "react-router-dom";
 import GreenButton from "./UI/buttons/GreenButton";
 import GreyButton from "./UI/buttons/GreyButton";
 
+const dealInfoDummyData = [
+  {
+    name1: "John",
+    name2: "Alice",
+    expired: false,
+    newDeal: true,
+    closedDeal: false,
+  },
+  {
+    name1: "Bob",
+    name2: null,
+    expired: true,
+    newDeal: false,
+    closedDeal: false,
+  },
+  {
+    name1: "Michael",
+    name2: "Sarah",
+    expired: true,
+    newDeal: false,
+    closedDeal: false,
+  },
+  {
+    name1: "David",
+    name2: "Emily",
+    expired: false,
+    newDeal: false,
+    closedDeal: true,
+  },
+];
+
+const clientDummyData = [
+  {
+    name: "John Doe",
+    photo: "photo",
+    likedAddresses: [
+      {
+        address: "123 Main St",
+      },
+      {
+        address: "456 Elm St",
+      },
+    ],
+    documents: [
+      {
+        title: "Document 1",
+      },
+      {
+        title: "Document 2",
+      },
+      {
+        title: "Document 3",
+      },
+    ],
+  },
+  {
+    name: "Jane Smith",
+    photo: "photo",
+    likedAddresses: [
+      {
+        address: "789 Oak St",
+      },
+      {
+        address: "101 Pine St",
+      },
+    ],
+    documents: [
+      {
+        title: "Document A",
+      },
+      {
+        title: "Document B",
+      },
+    ],
+  },
+];
+
 function DealsTab() {
   const navigate = useNavigate();
 
@@ -18,89 +95,161 @@ function DealsTab() {
   return (
     <div className="DealsTab">
       <div className="DealsTab_Container">
-        <div className="deals_details">
-          <div className="new_deals">
-            <h2>New Deals</h2>
-            <p>content</p>
+        <div className="deal_info">
+          <table className="new_deals">
+            <thead>
+              <tr>
+                <th colSpan="2" id="new_deals_title">
+                  New Deals
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {dealInfoDummyData.map(
+                (deal, index) =>
+                  deal["newDeal"] && (
+                    <tr key={index}>
+                      <td>
+                        {deal.name1} {deal.name2 ? `& ${deal.name2}` : ""}
+                      </td>
+                    </tr>
+                  )
+              )}
+            </tbody>
+          </table>
+          <table className="expired_deals">
+            <thead>
+              <tr>
+                <th colSpan="2" id="expired_deals_title">
+                  Expired Deals
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {dealInfoDummyData.map(
+                (deal, index) =>
+                  deal["expired"] && (
+                    <tr key={index}>
+                      <td>
+                        {deal.name1} {deal.name2 ? `& ${deal.name2}` : ""}
+                      </td>
+                    </tr>
+                  )
+              )}
+            </tbody>
+          </table>
+          <table className="closed_deals">
+            <thead>
+              <tr>
+                <th colSpan="2" id="closed_deals_title">
+                  Closed Deals
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {dealInfoDummyData.map(
+                (deal, index) =>
+                  deal["closedDeal"] && (
+                    <tr key={index}>
+                      <td>
+                        {deal.name1} {deal.name2 ? `& ${deal.name2}` : ""}
+                      </td>
+                    </tr>
+                  )
+              )}
+            </tbody>
+          </table>
+        </div>
+        <div className="right_box">
+          <div className="notifications">
+            <h4>2 New Deals</h4>
+            <h4>5 New Messages</h4>
+            <h4>1 Upcoming Dates</h4>
           </div>
-          <div className="expired_deals">
-            <h2>Expired Deals</h2>
-            <p>content</p>
-          </div>
-          <div className="closed_deals">
-            <h2>Closed Deals</h2>
-            <p>content</p>
+          <div className="active_deals">
+            <div id="active_deals_title">
+              <h3>Active Deals</h3>
+            </div>
+            <table className="contact_table">
+              <thead>
+                <tr>
+                  <th colSpan="2">Last Active: 2 hours ago</th>
+                  <th className="category_title">Control</th>
+                  <th className="category_title">Liked</th>
+                  <th colSpan="3" className="category_title">
+                    Documents
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {clientDummyData.map((deal, index) => (
+                  <tr key={index}>
+                    <td colSpan="2" style={{ verticalAlign: "top" }}>
+                      <div className="client_id">
+                        <p>{deal.photo}</p>
+                        <p>{deal.name}</p>
+                      </div>
+                      <div className="progress_bar">
+                        <p>Progress Bar</p>
+                        <p>_______</p>
+                      </div>
+                      <div onClick={sendMessage} className="chat_button">
+                        <GreenButton>Chat</GreenButton>
+                      </div>
+                    </td>
+                    <td style={{ verticalAlign: "top" }}>
+                      <div className="control_container">
+                        <form action="">
+                          <select
+                            name="process"
+                            id="process"
+                            className="select_control"
+                          >
+                            <option value="preapp">Pre Approved</option>
+                            <option value="approval">Approval</option>
+                            <option value="denied">Denied</option>
+                          </select>
+                        </form>
+                        <div className="load_documents">
+                          <GreyButton id="grey_button">
+                            Upload Documents
+                          </GreyButton>
+                          <GreyButton id="grey_button">
+                            Download Documents
+                          </GreyButton>
+                        </div>
+                        <p>Cal</p>
+                      </div>
+                    </td>
+                    <td
+                      className="liked_addresses cell_with_addresses"
+                      style={{ verticalAlign: "top", display: "table-cell" }}
+                    >
+                      <div className="address_container">
+                        {deal.likedAddresses.map((address, i) => (
+                          <div key={i} className="address_details">
+                            {address.address}
+                          </div>
+                        ))}
+                      </div>
+                    </td>
+                    <td
+                      className="documents_container"
+                      style={{ verticalAlign: "top", display: "table-cell" }}
+                    >
+                      {deal.documents.map((document, i) => (
+                        <div key={i} className="document_info">
+                          <p>{document.title}</p>
+                          <p>see</p>
+                        </div>
+                      ))}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
-        <div className="important_info">
-          <h4>2 New Docs</h4>
-          <h4>5 New Messages</h4>
-          <h4>1 Upcoming Dates</h4>
-        </div>
-        <div className="active_deals">
-          <h2>Active deals</h2>
-          <div className="active_deals_container">
-            <div className="lead_details">
-              <p className="last_active">active 2 hours ago</p>
-              <div className="name_image">
-                <p>image</p>
-                <p>John Smith</p>
-              </div>
-              <div className="progress_bar">
-                <p>progress bar</p>
-                <hr />
-              </div>
-              <GreenButton onClick={sendMessage}>Chat (!)</GreenButton>
-            </div>
-            <div className="controls">
-              <div className="controls_title">
-                <h3>control</h3>
-              </div>
-              <form action="">
-                <select name="process" id="process">
-                  <option value="preapp">Pre Approved</option>
-                  <option value="approval">Approval</option>
-                </select>
-              </form>
-              <GreyButton>Upload Documents</GreyButton>
-              <div className="download_docs">
-                <GreyButton>Download Documents</GreyButton>
-                <p>(!)</p>
-              </div>
-              <div className="calendar">
-                <p>cal</p>
-                <p>(!)</p>
-              </div>
-            </div>
-            <div className="liked_props">
-              <div className="liked_title">
-                <h3>liked</h3>
-              </div>
-              <p>123 Main St</p>
-              <p>345 Capital St</p>
-            </div>
-            {/* SUB WOKRING -----------------*/}
-            <div className="documents">
-              <div className="documents_title">
-                <h3>documents</h3>
-              </div>
-              <div>
-                <p>Tax Return 2023</p> <p>see</p>
-              </div>
-              <div>
-                <p>Proof of Identity</p> <p>see</p>
-              </div>
-              <div>
-                <p>Pre-Approval</p> <p>see</p>
-              </div>
-              <div>
-                <p>Proof of Identity</p> <p>see</p>
-              </div>
-            </div>
-            {/* SUB WOKRING -----------------*/}
-          </div>
-        </div>
-        {/* current work area */}
       </div>
     </div>
   );
