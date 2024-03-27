2fbb06482fa8bc1e61774531c07e1cce61011f46
bethjm <b.jeanne.mills@gmail.com>
2024-03-27 14:26:29 -0400

    have inital map on. looking into switching to googlegeocode map

--- src/components/Territory.js
+++ src/components/Territory.js
@@ -1,13 +1,35 @@
 import React from "react";
-
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import "./Territory.css";
+
+const position = [42.36, -71.05]; // Latitude and Longitude of the map's center
 
 function Territory() {
   return (
     <div className="Territory">
-      <div className="map">map</div>
+      {/* ------------------------------------------------------------*/}
+
+      <div className="map">
+        <MapContainer
+          center={position}
+          zoom={13}
+          style={{ height: "500px", width: "500px" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+      {/* ------------------------------------------------------------*/}
+
       <div className="zip_codes">
-        {/* current work area */}
         <div className="your_zip">
           <div className="your_zip_title">Your Territory</div>
           <table>
@@ -33,7 +55,6 @@ function Territory() {
             </tr>
           </table>
         </div>
-        {/* current work area */}
         <div className="buy_zip">
           <div className="buy_zip_title">
             <h3>Add Territory</h3>
